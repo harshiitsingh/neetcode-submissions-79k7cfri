0cbb31aec3d8dfd739b039105b6e75b1f583127f
Harshit Singh <77427143+harshiitsingh@users.noreply.github.com>
2026-04-20 11:33:06 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    bool alphaNum(char c){
+        return(c >= 'A' && c<= 'Z' ||
+                c>='a' && c<='z' ||
+                c>= '0' && c<='9');
+    }
+
+    bool isPalindrome(string s) {
+        int start=0, end=s.length()-1;
+        while(start<end){
+            while(start<end && !alphaNum(s[start])) start++;
+            while(start<end && !alphaNum(s[end])) end--;
+            if(tolower(s[start]) != tolower(s[end])){
+                return false;
+            }
+            start++; end--;
+        }
+        return true;
+    }
+};
